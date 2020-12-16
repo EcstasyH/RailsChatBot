555bd8f1bc0cb91a828ea58fc4d466f22f2132fc
EcstasyH <wuhao164@mails.ucas.ac.cn>
2020-12-16 20:32:25 +0800

    most done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
     @@current_username = "default"
-    
+    config.time_zone = 'Beijing'
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "no repeated users" do
+    user1 = User.create(username:"test@mails.com")
+    user2 = User.create(username:"test@mails.com")
+    user1.save
+    assert_not user2.save
+  end
 end
